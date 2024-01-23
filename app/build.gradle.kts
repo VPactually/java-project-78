d30d51b37f0fb6b3138e48f396005b1eeed8b754
Vladislav Pomozov <vpactually@gmail.com>
2024-01-23 10:30:45 +0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.4"
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
