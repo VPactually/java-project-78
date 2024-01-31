fb11b3436f42c3609038805ff3208e10d06b43fd
Vladislav Pomozov <vpactually@gmail.com>
2024-01-31 10:44:22 +0300

    fix hexlet test errors

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,9 +3,9 @@ package hexlet.code.schemas;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class MapSchema extends BaseSchema<Map<String, Object>> {
+public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
 
-    public MapSchema shape(Map<String, ? extends BaseSchema> schemasMap) {
+    public MapSchema<T> shape(Map<String, ? extends BaseSchema<T>> schemasMap) {
         addPredicate(o -> o == null || (new HashMap<>(o)).entrySet()
                 .stream()
                 .allMatch(stringObjectEntry -> {
@@ -16,7 +16,7 @@ public final class MapSchema extends BaseSchema<Map<String, Object>> {
         return this;
     }
 
-    public MapSchema sizeof(int sizeof) {
+    public MapSchema<T> sizeof(int sizeof) {
         addPredicate(o -> o == null || new HashMap<>(o).size() == sizeof);
         return this;
     }
